14d87255e2ecf44f9cf70f8ddba1e37febc85649
Anna Zolotareva <anazolotarewa@yandex.ru>
2021-11-26 10:29:47 +0400

    присвоила переменным значения

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-let title, screens, screenPrice, rollback, fullPrice, adaptive;
+let title = "projectFirst",
+  screens = "Простые, Сложные, Интерактивные",
+  screenPrice = 10,
+  rollback = 55,
+  fullPrice = 300000,
+  adaptive = true;
 alert("Первый урок");
 console.log("Первый урок");
+let myVar = 10;
+console.log(typeof myVar);
