a0b372a9f4c5a23a7bc131db7c43460bcea9f9ef
Anna Zolotareva <anazolotarewa@yandex.ru>
2021-12-01 00:11:11 +0400

    выполнила третье задание

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+"use strict";
 let title = "projectFirst",
   screens = "Простые, Сложные, Интерактивные",
   screenPrice = 10,
@@ -14,3 +15,40 @@ console.log(
 screens = screens.toLowerCase();
 console.log(screens.split(", "));
 console.log(fullPrice * (rollback / 100) + " %");
+// 3 задание
+title = prompt("Как называется Ваш проект?");
+screens = prompt("Какие типы экранов нужно разработать?");
+screenPrice = +prompt("Сколько будет стоить данная работа?");
+adaptive = prompt("Нужен ли адаптив на сайте?");
+switch (adaptive) {
+  case "да":
+  case "Да":
+  case "Yes":
+  case "yes":
+    adaptive = true;
+    break;
+  case "нет":
+  case "Нет":
+  case "No":
+  case "no":
+    adaptive = false;
+    break;
+  default:
+    adaptive = false;
+}
+let service1 = prompt("Какой дополнительный тип услуги нужен?");
+let servicePrice1 = +prompt("Сколько это будет стоить?");
+let service2 = prompt("Какой дополнительный тип услуги нужен?");
+let servicePrice2 = +prompt("Сколько это будет стоить?");
+fullPrice = screenPrice + servicePrice1 + servicePrice2;
+let servicePercentPrice = fullPrice - rollback;
+console.log(Math.ceil(servicePercentPrice));
+if (fullPrice >= 30000) {
+  console.log("Даем скидку в 10%");
+} else if (fullPrice >= 15000 && fullPrice < 30000) {
+  console.log("Даем скидку в 5%");
+} else if (fullPrice >= 0 && fullPrice < 15000) {
+  console.log("Скидка не предусмотрена");
+} else if (fullPrice < 0) {
+  console.log("Что то пошло не так");
+}
